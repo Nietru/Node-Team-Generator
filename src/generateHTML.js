50fa0c58437855de78b81d670666ce93339a3646
Tifannie Truman <nietru143@gmail.com>
2023-03-16 14:42:13 -0400

    worked with a tutor to figure out html generation and bootstrap and start test

---
+++ src/generateHTML.js
@@ -0,0 +1,75 @@
+function generateHTML(arr) {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+      crossorigin="anonymous"
+    />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid bg-danger">
+      <div class="container">
+        <h1 class="display-4 text-white center-text">Employee Generator</h1>
+      </div>
+    </div>
+
+    <div class="container">
+      ${structureCards(arr)}
+    </div>
+  </body>
+</html>
+`;
+}
+
+function structureCards(arr) {
+  let template = ``;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].getRole() == "Manager") {
+      template += `<div class="card text-center" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-header bg-info">Manager</h5>
+          <h5 class="card-title">Name: ${arr[i].getName()}</h5>
+          <h5 class="card-title">Office number: ${arr[i].getOfficeNum()}</h5>
+          <hr />
+          <p class="card-text">id: ${arr[i].getId()}</p>
+          <hr />
+          <a href="#" class="btn btn-primary">email: ${arr[i].getEmail()}</a>
+        </div>
+      </div>`;
+    } else if (arr[i].getRole() == "Engineer") {
+      template += `<div class="card text-center" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-header bg-success">Engineer</h5>
+          <h5 class="card-title">Name: ${arr[i].getName()}</h5>
+          <h5 class="card-title">Github: ${arr[i].getGithub()}</h5>
+          <hr />
+          <p class="card-text">id: ${arr[i].getId()}</p>
+          <hr />
+          <a href="#" class="btn btn-primary">email: ${arr[i].getEmail()}</a>
+        </div>
+      </div>`;
+    } else {
+      template += `<div class="card text-center" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-header bg-secondary">Inter</h5>
+          <h5 class="card-title">Name: ${arr[i].getName()}</h5>
+          <h5 class="card-title">Schoole: ${arr[i].getSchool()}</h5>
+          <hr />
+          <p class="card-text">id: ${arr[i].getId()}</p>
+          <hr />
+          <a href="#" class="btn btn-primary">email: ${arr[i].getEmail()}</a>
+        </div>
+      </div>`;
+    }
+  }
+  return template;
+}
+
+module.exports = generateHTML;
---
+++ tests/employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require("../lib/employee");
+
+test("It should create an object when you make any class", () => {
+  const testVariable = new Employee();
+
+  expect(typeof testVariable).toBe("object");
+});
+
+test("It should allow me to input a name, and get a name back", () => {
+  const testVariable = new Employee("James");
+
+  expect(testVariable.name).toBe("James");
+});
+
+test("It should allow me to input a name, and get a name back with a function", () => {
+  const testVariable = new Employee("James");
+
+  expect(testVariable.getName()).toBe("James");
+});
+
+test("It should allow me to input a id, and get a id back", () => {
+  const testVariable = new Employee("", "123");
+
+  expect(testVariable.id).toBe("123");
+});
+
+test("It should allow me to input a id, and get a id back with a function", () => {
+  const testVariable = new Employee("", "123");
+
+  expect(testVariable.getId()).toBe("123");
+});
