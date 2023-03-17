f9740591b75c5e65c382c3c44c5e046163eae205
Tifannie Truman <nietru143@gmail.com>
2023-03-17 16:55:31 -0400

    finished with testing with specific intern info test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,6 +1,6 @@
 const Engineer = require("../lib/engineer.js");
 
-test("It should allow me to input their github username and get the username back", () => {
+test("It should allow me to input their github username and get it back with a function", () => {
   const testGithub = new Engineer("", "", "", "github");
 
   expect(testGithub.getGithub()).toBe("github");
---
+++ tests/intern.test.js
@@ -0,0 +1,7 @@
+const Intern = require("../lib/intern.js");
+
+test("It should allow me to input a school name, and get it back with a function", () => {
+  const testSchool = new Intern("", "", "", "school");
+
+  expect(testVariable.getSchool()).toBe("school");
+});
